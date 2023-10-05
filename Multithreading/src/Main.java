385fb3147eff95e91797cd420751602cbfedb1cf
HugoSanche <hbmclaren@gmail.com>
2023-10-04 23:25:06 -0600

    first commit

---
+++ Multithreading/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException{
+       Thread thread=new Thread(new Runnable() {
+           @Override
+           public void run() {
+               System.out.println("We are now in thread "+Thread.currentThread().getName());
+               System.out.println("Current thread priority is "+Thread.currentThread().getPriority());
+           }
+       });
+
+       thread.setName("My Thread");
+       thread.setPriority(Thread.MAX_PRIORITY);
+        System.out.println("We are in thread "+Thread.currentThread().getName()+ " before starting a new thread");
+
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler(){
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A critical error happened in thread "+t.getName()+" error is "+e.getMessage());
+            }
+        });
+        
+        thread.run();
+        System.out.println("We are in thread "+Thread.currentThread().getName()+ " after starting a new thread");
+
+    }
+}
